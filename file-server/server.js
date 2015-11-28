bb5a22430c61154aa9a4d0486105c872e1391778
Kanit Mekritthikrai <sonkanit@yahoo.com>
2015-11-28 13:19:01 +0700

    Initial commit

---
+++ file-server/server.js
@@ -0,0 +1,14 @@
+console.log('hello');
+var http = require('http');
+const PORT=8080;
+
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
